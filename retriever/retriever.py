00da392c2c28fba0a114e16f8751839f1d1224a5
Harouak24 <m.harouak@aui.ma>
2025-02-07 12:58:47 +0100

    m

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -1,10 +1,11 @@
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 def query_faiss(query):
-    embeddings = OpenAIEmbeddings()
+    API_KEY = "OPENAI_API_KEY"
+    embeddings = OpenAIEmbeddings(openai_api_key = API_KEY)
     vectorstore = FAISS.load_local("data/faiss_index", embeddings)
     qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0), retriever=vectorstore.as_retriever())
     return qa_chain.run(query)
